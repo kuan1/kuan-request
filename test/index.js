15faca22d3fb6128a627e924687f6a426f074002
luzhongkuan <luzhongk@126.com>
2018-10-11 00:07:31 +0800

    readme

--- test/index.js
+++ test/index.js
@@ -1,7 +1,16 @@
 import Requeset from '../src'
 
-const request = (new Requeset()).create()
+const requestObj = new Requeset({
+  // getHeaders：自定义headers
+  handleError: (status, msg, error) => console.log(status, msg, error), // 自定义错误处理
+  // loading = loadingCtrl, // 自定义loading
+  // toast = Toast // 自定义提示
+})
+
+const request = requestObj.create()
 
 request({
-  url: 'http://api.luzhongkuan.cn/api/location2'
+  url: 'http://api.luzhongkuan.cn/api/location2',
+  loading: false,
+  alert: false
 })
