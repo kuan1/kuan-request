5e42648eae5e8b18d5b67e96859f770192e4403c
luzhongkuan <luzhongk@126.com>
2018-10-15 15:17:59 +0800

    打包

--- build.js
+++ build.js
@@ -1,5 +1,10 @@
 process.env.NODE_ENV = 'production'
+process.env.EXTRACT_CSS = '0'
+process.env.DISCARD_HTML = '0'
 
+const path = require('path')
 const { buildLib: build } = require('kuan-pack')
 
-build()
+build({
+  distPath: path.resolve(__dirname, 'lib')
+})
